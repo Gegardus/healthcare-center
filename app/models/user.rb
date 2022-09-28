33522c7c5529972cac9e305d062d165cfd46199b
Gegardus <stgegard@gmail.com>
2022-09-28 13:26:35 +0400

    add associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :appointments, dependent: :destroy
+  has_many :doctors, through: :appointments
 
   validates :phone, uniqueness: true
 
