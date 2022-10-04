e3a9dc6e57aaf83b0fd3ca72b7cbc66fd8fb1f17
Gegardus <stgegard@gmail.com>
2022-10-04 17:25:06 +0400

    update tables category, prescription

--- db/migrate/20220928063825_create_prescriptions.rb
+++ db/migrate/20220928063825_create_prescriptions.rb
@@ -1,10 +1,12 @@
 class CreatePrescriptions < ActiveRecord::Migration[7.0]
   def change
     create_table :prescriptions do |t|
-      t.text :prescript
-      t.references :appointment, null: false, foreign_key: true
+      t.belongs_to :appointment, foreign_key: true
+      t.text :prescript  
 
       t.timestamps
     end
   end
 end
+
+
