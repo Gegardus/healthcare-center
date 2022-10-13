8ca31787ff1a2d40e8c60dc74d536943ef9f3f18
Gegardus <stgegard@gmail.com>
2022-10-13 10:37:27 +0400

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  full_name: 'John Doe',
+  phone: '2222222222',
+  password: 'password2')
+
+User.create!(
+  full_name: 'Jane Doe',
+  phone: '3333333333',
+  password: 'password3')  
+
+p "Patients are successfully created!"
+
+AdminUser.create!(
+  full_name: 'John Boss',
+  phone: '1111111111',
+  password: 'password1')
+
+p "Admin is successfully created!"
