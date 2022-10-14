a1e0af7a28cd228bed3a3ce187919b4fbf485ce9
Gegardus <stgegard@gmail.com>
2022-10-14 20:50:37 +0400

    fix feature test

--- spec/features/new_prescription_spec.rb
+++ spec/features/new_prescription_spec.rb
@@ -2,26 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Create Prescription', type: :feature do
   before(:each) do
-    create(:category)
-    @doctor = create(:doctor)
+    Doctor.destroy_all
+    User.destroy_all
+    Category.destroy_all
+    Appointment.destroy_all
+    @category = create(:category)
     @user = create(:user)
+    @doctor = create(:doctor)
     @appointment = create(:appointment)
-    create(:prescription)
-
     visit new_doctor_session_path
     fill_in 'Phone', with: @doctor.phone
     fill_in 'Password', with: @doctor.password
     click_button 'Log in'
     visit doctor_path(@doctor.id)
-    click_link('Write a prescript')
+    expect(page).to have_content 'Signout'
   end
 
   describe 'prescriptions#new' do
     it 'adds data to the form and sucessufully submits' do
-      visit new_prescription_path
+      visit new_prescription_path(appointment_id: @appointment.id)
       fill_in 'Prescript', with: 'Ipsum Ipsum Ipsum'
       click_on 'Submit'
-      expect(page).to have_field('prescript')
+      expect(page).to have_content 'All Prescriptions'
     end
   end
 end
